ea205097cf2b7e8bd5bab3acb299e29cb39770be
Daniel Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2019-10-09 07:19:49 +0200

    Update Startup.cs

--- CarsIsland/cars-island-web-api/CarsIsland/CarsIsland.WebAPI/Startup.cs
+++ CarsIsland/cars-island-web-api/CarsIsland/CarsIsland.WebAPI/Startup.cs
@@ -61,7 +61,6 @@ namespace CarsIsland.WebAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseMvc();
         }
